2e1e23e9aae2663c1128c374635403a809a6fa81
skondo <s.kondo11235813@gmail.com>
2022-07-30 13:07:36 +0900

    mozcの導入方法を変更

--- script/install_mozc_ubuntu.sh
+++ script/install_mozc_ubuntu.sh
@@ -1,1 +1,6 @@
-sudo apt -y install ibus-mozc
+sudo add-apt-repository ppa:ikuya-fruitsbasket/fcitx5
+sudo apt upgrade
+sudo apt install fcitx5-mozc
+im-config -n fcitx5
+
+echo "RUN sudo apt -y purge ibus"
