077478344f0cb93b92428690e9f8760cb66083d3
skondo <s.kondo11235813@gmail.com>
2022-10-07 12:52:56 +0900

    fix

--- script/update.sh
+++ script/update.sh
@@ -4,7 +4,7 @@ git -C $LOCAL_SETTINGS pull origin master
 
 case ${OSTYPE} in
   darwin*)
-    $LOCAL_BIN/nvim-osx64/bin/nvim --headless +so $XDG_CONFIG_HOME/nvim/init.vim +qa
+    $LOCAL_BIN/nvim-macos/bin/nvim --headless +so $XDG_CONFIG_HOME/nvim/init.vim +qa
     ;;
   linux*)
     $LOCAL_BIN/nvim.appimage --headless +so $XDG_CONFIG_HOME/nvim/init.vim +qa
